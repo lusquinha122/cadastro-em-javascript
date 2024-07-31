7dc48d062a8e0d7d8c99c1fe1e75b92d9117ed3c
lusquinha122 <134716400+lusquinha122@users.noreply.github.com>
2024-07-31 10:45:32 -0300

    Update sistema_de_cadastro.js

--- cadastro em javascritp/sistema_de_cadastro.js
+++ cadastro em javascritp/sistema_de_cadastro.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
+const readline = require('readline');
+const crypto = require('crypto');
+
+const rl= readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
 
 function cadastrar(nome, idade, email, telefone) {
     const dados = `${nome};${idade};${email};${telefone}\n`;
@@ -9,6 +18,30 @@ function cadastrar(nome, idade, email, telefone) {
         console.log('Cadastro realizado com sucesso!');
       }
     });
+  }
+
+  function login() {
+    rl.question('digite email: ' , (resposta) => {
+      email = resposta;
+      rl.question('digite a senha ' , (resposta) => {
+        senha = crypto.createHash('sha256').update(resposta).digest('hex');
+        fs.readline('usuarios.txt', 'utf8', (err, data) => {
+          if (err) {
+        console.error('erro ao ler arquivo de usuarios', err);
+          } else {
+            const usuarios = data.split('\n');
+            for (const usuario of usuarios) {
+              const [nome, emailusuario, telefone, idade, senhausuario] = usuario.split(';');
+              if (email === emailusuario && senha === senhausuario) {
+                console.log ('login efetuado com sucesso');
+                return;
+              }
+            }
+            console.log ('email ou senha incorretos')
+          }
+        })
+      })
+    })
   }
 
   function listarCadastros() {
@@ -25,26 +58,24 @@ function cadastrar(nome, idade, email, telefone) {
     });
   }
 
-  const readline = require('readline');
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
   
   rl.question('Digite 1 para cadastrar ou 2 para listar cadastros: ', (resposta) => {
     if (resposta === '1') {
       rl.question('Digite o nome: ', (nome) => {
         rl.question('Digite a idade: ', (idade) => {
           rl.question('Digite o e-mail: ', (email) => {
+            rl.question('Digite a senha: ', (senha) => {
+              senha = crypto.createHash('sha256').update(resposta).digest('hex')
             rl.question('Digite o telefone: ', (telefone) => {
-              cadastrar(nome, idade, email, telefone);
+              cadastrar(nome, idade, email, senha, telefone);
               rl.close();
             });
           });
         });
       });
-      } else if (resposta === '2') {
+     });
+    } 
+      else if (resposta === '2') {
           listarCadastros();
           rl.close();
       } else {
